90604634bf6db5ac9281a9685290bdf2c7179bdc
Ingez <ingezero1@gmail.com>
2022-06-06 23:09:17 -0500

    Se agrega el NavBar y se estructura el estilo de manera basica con solo CSS

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "./navbar.css"
+
+function NavBar() {
+
+    return (
+        <nav>
+            <h2>Tienda Polimnia Reposteria</h2>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Carrito</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
